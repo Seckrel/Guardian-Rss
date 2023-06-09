a0f1b36579aab8a513825ed89a2c8b14612e1ac0
Aayam <aayam.oza@gmail.com>
2023-06-09 21:18:58 +0545

    inital test router setup for guardian completed

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-// import Router from "./router";
+import { guardianRouter } from "./routers/guardianRouter.js";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  console.log("working");
-  return res.send({ msg: "hello" });
-});
+// app.get("/", (req, res) => {
+//   console.log("working");
+//   return res.send({ msg: "hello" });
+// });
 
-// try {
-//   const router = new Router();
-
-//   app.use("/v1", router.getRoutes);
-// } catch (err) {
-//   console.log("error", err);
-// }
+app.use("/v1/rss-feed", guardianRouter);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
