26e41d97dfa3d1670932620ffebd4b0194dd5937
Aayam <aayam.oza@gmail.com>
2023-06-11 14:17:31 +0545

    prisma connected with postgresql and creating logger table for logging

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-
+console.log(process.env.POSTGRES_URL)
 app.use("/v1/rss-feed", guardianRouter); // API that handles all The Guardian RSS feed related controllers
 
 app.listen(port, () => {
